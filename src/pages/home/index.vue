745d792759b7e9b6ad83adb4ef31cfaf01a79e1c
yq <447845126@qq.com>
2021-01-15 18:10:08 +0800

    chore: 首页创建

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,17 @@
 <template lang='pug'>
-  .page-home home
+  .page-home
+    h1 @yqcoder_components
+    
+    .page-home_step 
+      p 1、安装依赖
+      .page-home_step-box npm install @yqcoder/components
+    
+    .page-home_step 
+      p 2、main.js引用依赖
+      .page-home_step-box 
+        .page-home_step-box import yqComponent from "@yqcoder/components";
+        .page-home_step-box Vue.use(yqComponent);
+
 </template>
 
 <script>
@@ -7,3 +19,7 @@ export default {
   name: 'page-home'
 }
 </script>
+
+<style lang='scss'>
+@import "./index.scss";
+</style>
